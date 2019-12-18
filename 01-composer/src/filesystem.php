190e8c73762cc8ed6bdc4a2de3ce5e3b4c39ec96
Fábio Souza Teixeira <395063@sga.pucminas.br>
2019-12-17 21:57:46 -0300

    first commit

--- 01-composer/src/filesystem.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App;
-
-class Filesystem {
-    public function write(string $level, string $message): void {
-        file_put_contents('app.log', sprintf("[%s] %s", $level, $message));
-    }
-}
